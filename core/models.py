e0e24dfef7db4f506ce616eab83065b28688afe4
Casey <crrauser@quinnipiac.edu>
2015-11-10 19:30:58 +0000

    Add question create view

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Question(models.Model):
+  title = models.CharField(max_length=200)
+  description = models.TextField(null=True, blank=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+  user = models.ForeignKey(User)
+  
+  def __unicode__(self):
+    return self.title
