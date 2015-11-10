11e3f4e22e527d636688e4daf4419b4d68db46d2
Casey <crrauser@quinnipiac.edu>
2015-11-10 19:50:58 +0000

    Add question detail view

--- core/models.py
+++ core/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 # Create your models here.
 class Question(models.Model):
   title = models.CharField(max_length=200)
   description = models.TextField(null=True, blank=False)
   created_at = models.DateTimeField(auto_now_add=True)
   user = models.ForeignKey(User)
-  
+
   def __unicode__(self):
     return self.title
+  def get_absolute_url(self):
+    return reverse("question_detail", args=[self.id])
